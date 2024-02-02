44d69f4a881b0bcf361953cde8cd653b69aee0c6
Quramy <yosuke.kurami@gmail.com>
2024-02-02 16:33:19 +0900

    update

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
 import express from "express";
 
-async function main() {
-  const app = express();
+const app = express();
 
-  app.get("/api", async (_, res) => {
-    const result = await fetch("https://google.com");
-    res.json({
-      status: result.status,
-    });
+app.get("/api", async (_, res) => {
+  const result = await fetch("https://github.com");
+  res.json({
+    status: result.status,
   });
+});
 
-  app.listen(7070, () => {
-    console.log("start", "http://localhost:7070");
-  });
-}
-
-main();
+app.listen(7070, () => {
+  console.log("start", "http://localhost:7070");
+});
